02aff9fad3af9b2003cbaec72de7ca10f82bb24a
jamesledev <74250657+jamesledev@users.noreply.github.com>
2020-12-31 13:40:35 +0000

    adding react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello my name is James, this is the react test
+      </header>
+    </div>
+  );
+}
+
+export default App;
